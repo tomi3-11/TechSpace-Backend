94538785742615d7060a797accd08b411466bf4c
thomas <didymusthomas6@gmail.com>
2025-12-19 21:53:22 +0300

    Update to add automatic tables creation

---
+++ run.py
@@ -0,0 +1,17 @@
+from app import create_app, db
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = create_app()
+
+@app.cli.command("create-db")
+def create_db():
+    """Create all the tables."""
+    with app.app_context():
+        db.create_all()
+        print("Database Tables created.")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
