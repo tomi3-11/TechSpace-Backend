a63fdb7e156c4dad6709c666f86d0a03d5574b1b
thomas <didymusthomas6@gmail.com>
2025-12-21 04:05:49 +0300

    Add the feeds resource

---
+++ app/blueprints/feeds/resources.py
@@ -0,0 +1,67 @@
+from flask import request, jsonify
+from flask_restful import Resource
+from app.blueprints.feeds.service import FeedService
+
+
+def feed_response(pagination):
+    return jsonify({
+        "total": pagination.total,
+        "page": pagination.page,
+        "pages": pagination.pages,
+        "results": [
+            post.to_dict() for post in pagination.items
+        ]
+    })
+    
+    
+class LatestFeedResource(Resource):
+    def get(self):
+        page = request.args.get("page", 1, int)
+        per_page = request.args.get("per_page", 20, int)
+        post_type = request.args.get("type")
+        
+        data = FeedService.latest(
+            page, per_page, post_type
+        )
+        
+        return feed_response(data)
+    
+    
+class TopFeedResource(Resource):
+    def get(self):
+        page = request.args.get("page", 1, int)
+        per_page = request.args.get("per_page", 20, int)
+        
+        return feed_response(
+            FeedService.top(page, per_page)
+        )
+        
+        
+class TrendingFeedResource(Resource):
+    def get(self):
+        page = request.args.get("page", 1, int)
+        per_page = request.args.get("per_page", 20, int)
+        
+        return feed_response(
+            FeedService.trending(page, per_page)
+        )
+        
+        
+class ProposalFeedResource(Resource):
+    def get(self):
+        page = request.args.get("page", 1, int)
+        per_page = request.args.get("per_page", 20, int)
+        
+        return feed_response(
+            FeedService.proposals(page, per_page)
+        )
+        
+        
+class CommunityFeedResource(Resource):
+    def get(self, slug):
+        page = request.args.get("page", 1, int)
+        per_page = request.args.get("per_page", 20, int)
+        
+        return feed_response(
+            FeedService.community(slug, page, per_page)
+        )
