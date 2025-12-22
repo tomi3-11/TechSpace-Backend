998a343495dd6be3f6b83f1b044a539c271260f3
thomas <didymusthomas6@gmail.com>
2025-12-22 04:44:26 +0300

    Update routes

--- app/blueprints/project_application/routes.py
+++ app/blueprints/project_application/routes.py
@@ -1,11 +1,10 @@
 from flask_restful import Api
-from .resources import ProjectApplicationCreateResource, SingleApplicationResource, CurrentUserApplicationsResource, ProjectReviewApplicationReviewResource
+from .resources import ProjectApplicationCreateResource, CurrentUserApplicationsResource, ProjectApplicationReviewResource
 
 def register_routes(bp):
     api = Api(bp)
     
     # Endpoints
-    api.add_resource(ProjectApplicationCreateResource, "/")
-    api.add_resource(ProjectReviewApplicationReviewResource, "/<int:project_id>/review/")
-    api.add_resource(SingleApplicationResource, "/<int:project_id>/")
+    api.add_resource(ProjectApplicationCreateResource, "/projects/<int:project_id>/applications/")
+    api.add_resource(ProjectApplicationReviewResource, "/projects/<int:project_id>/applications/<int:application_id>/review/")
     api.add_resource(CurrentUserApplicationsResource, "/mine/")
